8001f44f5eafacd5d774cb21b3069cf9a4c4f4b9
Billy Dunn <bdunn27@gmail.com>
2024-03-12 13:00:41 -0400

    advance lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (x) => {
+  return (fare) => {
+    return fare * x;
+  };
+};
+
+const fareDoubler = (createFareMultiplier) => createFareMultiplier * 2;
+
+const fareTripler = (createFareMultiplier) => createFareMultiplier * 3;
+
+const selectDifferentDrivers = (drivers, a) => {
+  return a(drivers);
+};
+const drivers = ["'Antonia', 'Nuru', 'Amari', 'Mo'"];
